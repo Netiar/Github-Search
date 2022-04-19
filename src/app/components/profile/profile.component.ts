35d830fddda7e01fa2f35a49d49d2ed65bda298d
Raindrop <collins.odinga@student.moringaschool.com>
2022-04-19 23:03:29 +0300

    modified profile.ts

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,20 +8,24 @@ import { ProfileService } from '../services/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile: any;
 
   constructor(private profileService:ProfileService) { 
     this.profileService.getProfileInfo().subscribe((profile: any) => {
       console.log(profile);
-  }
+      this.profile = profile;
+  });
 
-  ngOnInit(): void {
-  }
+  //this.ngOnInit()  {
+    ""
+  //}
 
 }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-  function ngOnInit(): any {
-    throw new Error('Function not implemented.');
-  }
+  
 
+  
+
+}
